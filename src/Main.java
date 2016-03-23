469995894bf078570fead3d50b3c3fabb22d1c78
hippo <hippo_cai@sina.com>
2016-03-23 20:56:19 +0800

    ssdas

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import javax.swing.UIManager;
 import com.ft9.dao.DAOer;
 import com.ft9.service.ServiceManager;
 import com.ft9.view.ViewManager;
-import com.ft9.view.frame.LoginDiagram;
 import com.ft9.view.frame.MainFrame;
 
 
@@ -16,19 +15,19 @@ public class Main {
 		// TODO 自动生成的方法存根
 		 try {  
 			 initWork();
-			 LoginDiagram lgD=new LoginDiagram();
-			 lgD.setVisible(true);	 
-			 lgD.setLoginSuccessfulActionListener(new LoginDiagram.LoginSuccessfulEventListener() {
-				@Override
-				public void afterLoginSuccessful() {
-					// TODO 自动生成的方法存根
-					MainFrame mainFrame=new MainFrame();
-  		            mainFrame.setVisible(true);
-				}
-			});
+//			 LoginDiagram lgD=new LoginDiagram();
+//			 lgD.setVisible(true);	 
+//			 lgD.setLoginSuccessfulActionListener(new LoginDiagram.LoginSuccessfulEventListener() {
+//				@Override
+//				public void afterLoginSuccessful() {
+//					// TODO 自动生成的方法存根
+//					MainFrame mainFrame=new MainFrame();
+//  		            mainFrame.setVisible(true);
+//				}
+//			});
 
-//			 MainFrame mainFrame=new MainFrame();
-//	         mainFrame.setVisible(true);
+			 MainFrame mainFrame=new MainFrame();
+	         mainFrame.setVisible(true);
           }catch(FileNotFoundException fe){
         	 fe.printStackTrace();
         	  JOptionPane.showMessageDialog(null, fe.getMessage(), "File Error", JOptionPane.ERROR_MESSAGE);
