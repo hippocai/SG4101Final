46fb4ed971cd191d38088474b5f7e15a0d22a1ba
hippo <hippo_cai@sina.com>
2016-04-03 22:25:11 +0800

    The test of vendor service

--- Test/unitTest/VendorServiceTest.java
+++ Test/unitTest/VendorServiceTest.java
@@ -6,10 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.ft9.bean.CategoryBean;
@@ -26,19 +23,12 @@ import com.ft9.service.impl.VendorService;
  * class description: Test the functions of VendorService <BR>
  * Remark: <BR>
  * @version 1.00 
- * @author Guo Qi
+ * @author CaiYicheng
  */
 public class VendorServiceTest {
 
 	IVendorService vendorService;
 	ICategoryService categoryService;
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
 
 	@Before
 	public void setUp() throws Exception {
@@ -48,9 +38,6 @@ public class VendorServiceTest {
 		categoryService=(CategoryService)ServiceManager.getService("Category");
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void test() {
@@ -58,6 +45,49 @@ public class VendorServiceTest {
 		vendorBean.setDescription("A Company");
 		vendorBean.setName("Test Vendor");
 		List<CategoryBean>allCategoryList=categoryService.getAllCategorys();
+		//if the number of category is insufficient,add some;
+		if(allCategoryList.size()==0){
+			CategoryBean categoryBean=new CategoryBean();
+			categoryBean.setCode("TST");
+			categoryBean.setName("Test Category");
+			categoryService.addCategoryByBean(categoryBean);
+		}
+		if(allCategoryList.size()==1){
+			CategoryBean categoryBean=new CategoryBean();
+			categoryBean.setCode("TST2");
+			categoryBean.setName("Test Category2");
+			categoryService.addCategoryByBean(categoryBean);
+		}
+		//the new vendor shouldn't be existed
+		assertFalse(vendorService.isNameExisted(vendorBean.getName()));
+		
+		assertTrue(vendorService.addNewVendorInCategoryBeanList(vendorBean, allCategoryList));
+		//add all the categories in to the vendor
+		List<CategoryBean>selectedCategoryList=vendorService.getAllCategoriesByVendorName(vendorBean.getName());
+		assertTrue(allCategoryList.size()==selectedCategoryList.size());
+		//check the search function
+		VendorBean searchedVendor=vendorService.searchVendorByKey("name", "est Vend").get(0);
+		assertEquals(vendorBean.getName(),searchedVendor.getName());
+		
+		CategoryBean deletedCategoryBean=selectedCategoryList.get(0);
+		selectedCategoryList.remove(0);
+		//check the update function
+		assertTrue(vendorService.updateVendorByCategoryList(vendorBean,selectedCategoryList));
+		assertTrue(vendorService.getAllCategoriesByVendorName(vendorBean.getName()).size()==allCategoryList.size()-1);
+		
+		List<VendorBean>newVendorList=vendorService.getVendorByCategory(deletedCategoryBean.getCode());
+		for(VendorBean vendorBeanInList:newVendorList){
+			//check the vendor have already been updated
+			assertFalse(vendorBeanInList.getName().equals(vendorBean.getName()));
+		}
+		//check the vendor delete function
+		for(CategoryBean categoryBean:selectedCategoryList){
+			assertTrue(vendorService.deleteVendorByNameInCategory(vendorBean.getName(), categoryBean.getCode()));
+		}
+		//check the vendor have already been deleted
+		Map<String,String>map=new HashMap<String,String>();
+		map.put("name", vendorBean.getName());
+		assertFalse(vendorService.deleteVendorByMap(map));
 	}
 
 }
